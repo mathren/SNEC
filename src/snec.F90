d156f809e06f70509a7f14403db016d6d402c9af
Mathieu Renzo <mrenzo@arizona.edu>
2026-01-20 14:55:53 -0700

    whitespaces and test of modification

--- src/snec.F90
+++ src/snec.F90
@@ -13,7 +13,7 @@ program snec
 !------------------------------------------------------------------------------
 
   write(*,*)
-  
+
   write(*,*) "***********************************"
   write(*,*) "* Supernova Explosion Code (SNEC) *"
   write(*,*) "***********************************"
@@ -21,7 +21,7 @@ program snec
   write(*,*)
 ! *****************************************************
 ! INITIALIZATION
-! *****************************************************        
+! *****************************************************
 
   call input_parser
 
@@ -33,7 +33,7 @@ program snec
   call output_all(0)
   call output_all(1)
   call output_all(2)
- 
+
   call timestep
 
   tdump_check = tstart+dtout_check
@@ -41,7 +41,7 @@ program snec
   tdump = tstart+dtout
   time = tstart
   nt = ntstart
-  
+
 
 ! *****************************************************
 ! MAIN LOOP
@@ -49,11 +49,11 @@ program snec
 
 
   IntegrationLoop: do
-     
+
      dtime_p = dtime
      ! determine dt
      call timestep
-     
+
 
      if(ntinfo.gt.0) then
         if(mod(nt,ntinfo).eq.0) then
@@ -61,20 +61,20 @@ program snec
            call outinfo
         endif
      endif
-     
+
      if((time+dtime).gt.tend) dtime = tend-time
 
      ! actual integration step
      call blstep
-     
+
      ! increment timestep
      nt = nt + 1
- 
+
      ! various output related things
      if (ntout.gt.0) then
         if ( mod(nt,ntout) .eq. 0) OutputFlag = .true.
      endif
-   
+
      if (ntout_scalar.gt.0) then
         if ( mod(nt,ntout_scalar) .eq. 0 ) OutputFlagScalar = .true.
      endif
@@ -82,7 +82,7 @@ program snec
      if (ntout_check.gt.0) then
         if ( mod(nt,ntout_check) .eq. 0 ) OutputFlagCheck = .true.
      endif
-     
+
      if ( time.ge.tdump) then
         tdump=tdump+dtout
         OutputFlag = .true.
@@ -97,21 +97,21 @@ program snec
         tdump_check=tdump_check+dtout_check
         OutputFlagCheck = .true.
      endif
-     
+
      ! increment time
      time = time+dtime
-     
+
      if (OutputFlag) then
         call output_all(0)
         call output_all(1)
         OutputFlag = .false.
      endif
-     
+
      if (OutputFlagScalar) then
         call output_all(2)
         OutputFlagScalar = .false.
      endif
-     
+
      if (OutputFlagCheck) then
         OutputFlagCheck = .false.
      endif
@@ -131,7 +131,7 @@ program snec
         call output_all(2)
         exit
      endif
-   
+
   enddo IntegrationLoop
 
 end program snec
