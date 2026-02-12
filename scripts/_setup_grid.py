43349cb33ba00a8bde5f1b9ac5b40ae315ee5f6d
Mathieu Renzo <mrenzo@arizona.edu>
2026-02-12 16:23:47 -0700

    minor

--- scripts/_setup_grid.py
+++ scripts/_setup_grid.py
@@ -1,7 +1,30 @@
-# script to setup grid of SNEC runs
+#!/usr/local/bin/python
+# Authors:
+#          Mathieu Renzo <mrenzo@arizona.edu>
+#
+# Keywords: files
+
+# Copyright (C) 2026 Mathieu Renzo
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+
+__author__ = ["Mathieu Renzo <mrenzo@arizona.edu>"]
+
 import os
 from MESAreader import get_src_col
 from short_and_iso import prepare_SNEC_input
+# from utilsLib import checkFolder, gitPush
 
 
 def get_num_cells(INPUT_PROFILE):
@@ -45,17 +68,17 @@ if __name__ == "__main__":
 
     # SNEC_ROOT="/home/u20/mrenzo/codes/SNEC/" # cluster_ua
     SNEC_ROOT = "/home/mrenzo/Documents/Research/codes/SNEC-1.01/" # ua_w
-    OUTDIR_ROOT='/tmp/SNEC_test/'
+    OUTDIR_ROOT='/home/mrenzo/Runs/SNEC_grid'
     INPUT_MESA_FILE = "/home/mrenzo/Runs/LMXRB/CCSN_progenitors/s30VdJNL_0.33_onset_cc.data"
-    final_energies = ["1.0d51", # 1.0d50, 1.0d49, 1.0d48, 1d47, 1d46, 1d45, 0.0d0, -1d45,
-                      "-1d46"]
+    # final_energies need to be strings including a decimal point and d for exponential notation, or SNEC will complain
+    final_energies = ["1.0d51", "1.0d50", "1.0d49", "1.0d48", "1.0d47", "1.0d46", "1.0d45", "0.0d0", "-1.0d45",  "-1.0d46",  "-1.0d44",  "-1.0d43"]
 
     # check if folder exists and user wants to erase it
     if os.path.isdir(OUTDIR_ROOT):
         print("Folder "+OUTDIR_ROOT+" exists!")
         go_on = input("Erase it and re-create it? [Y,y]")
         if go_on.lower() != "y":
-            exit()
+            sys.exit()
         else:
             print("Ok, cleaning up...")
             os.system("rm -rf "+OUTDIR_ROOT)
@@ -77,6 +100,11 @@ if __name__ == "__main__":
                         OUTDIR=OUTDIR,
                         FINAL_ENERGY=FINAL_ENERGY,
                         )
+
+        # copy SNEC executable -- assumes code is already compiled
+        os.system("cp "+SNEC_ROOT+"snec "+OUTDIR)
+
+
         print ("Done grid setup in")
         print("")
         print(OUTDIR_ROOT)
